4b873f009539a70c6d3ae95b74bb24909b17893d
luca1iu <lucaliu217@outlook.com>
2022-10-25 15:48:03 +0800

    finished

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from locations import Locations
+from forms import AddLocationForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRET_PROJECT'
@@ -11,39 +12,37 @@ UP_ACTION = "\u2197"
 DEL_ACTION = "X"
 
 
-
 @app.route("/<category>", methods=["GET", "POST"])
 def locations(category):
     locations = visit.get_list_by_category(category)
-    # Check the request for form data and process
-    if False:
-        [(name, action)] = [(None, None)]
+    ## Check the request for form data and process
+    if request.method == "POST":
+        [(name, action)] = request.form.items()
 
         if action == UP_ACTION:
             visit.moveup(name)
         elif action == DEL_ACTION:
             visit.delete(name)
-    # Return the main template with variables
-    return render_template("locations.html", category=category, categories=categories, locations=locations)
+    ## Return the main template with variables
+    return render_template("locations.html", category=category, categories=categories, locations=locations,
+                           add_location=AddLocationForm())
 
 
 @app.route("/add_location", methods=["POST"])
 def add_location():
     ## Validate and collect the form data
-
-    if True:
-        name = None
-        description = None
-        category = None
+    add_form = AddLocationForm()
+    if add_form.validate_on_submit():
+        name = add_form.name.data
+        description = add_form.description.data
+        category = add_form.category.data
         visit.add(name, description, category)
 
     ## Redirect to locations route function
-    return ""
+    return redirect(url_for("locations", category="recommended", _external=True, _scheme='https'))
 
 
 @app.route("/")
 def index():
     ## Redirect to locations route function
-    return ""
-
-
+    return redirect(url_for("locations", category="recommended", _external=True, _scheme='https'))
